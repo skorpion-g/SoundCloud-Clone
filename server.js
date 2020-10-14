e33f1100de8bd0d1f820540e6a12115ace669f1a
skorpion-g <frankieleeroberts@gmail.com>
2020-10-13 19:33:45 -0700

    "All services appear on DOM"

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const { truncate } = require('fs');
 var { createProxyMiddleware } = require('http-proxy-middleware');
-var port = 3000;
+var port = 7000;
 var bodyParser = require('body-parser');
 
 var app = express();
@@ -11,11 +11,38 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/songdata/', createProxyMiddleware({
   target: 'http://localhost:1000',
-  headers: {
-    method: 'GET'
-  },
   changeOrigin: true
 }));
+
+app.use('/relatedTracks/', createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrigin: true
+}))
+
+app.use('/artistBio/', createProxyMiddleware({
+  target: 'http://localhost:2000',
+  changeOrigin: true
+}))
+
+app.use('/hashtags/', createProxyMiddleware({
+  target: 'http://localhost:9001',
+  changeOrigin: true
+}))
+
+app.use('/comments/', createProxyMiddleware({
+  target: 'http://localhost:9000',
+  changeOrigin: true
+}))
+
+app.use('/users/', createProxyMiddleware({
+  target: 'http://localhost:9002',
+  changeOrigin: true
+}))
+
+app.use('/', createProxyMiddleware({
+  target: 'http://localhost:1000',
+  changeOrigin: true
+}))
 
 app.use('/', createProxyMiddleware({
   target: 'http://localhost:1000',
