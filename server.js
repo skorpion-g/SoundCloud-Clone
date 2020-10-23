c4d2c6965305e9b3ac004403aae9e15d9148ee2d
skorpion-g <frankieleeroberts@gmail.com>
2020-10-22 19:49:01 -0700

    "Changed to port 80"

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express');
 const { truncate } = require('fs');
 var { createProxyMiddleware } = require('http-proxy-middleware');
-var port = 7000;
+var path = require('path');
+var port = 80;
 var bodyParser = require('body-parser');
 
 var app = express();
 app.use(express.static('client'))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.use('/songdata/', createProxyMiddleware({
   target: 'http://localhost:1000',
@@ -25,32 +27,24 @@ app.use('/artistBio/', createProxyMiddleware({
 }))
 
 app.use('/hashtags/', createProxyMiddleware({
-  target: 'http://localhost:9001',
+  target: 'http://localhost:4001',
   changeOrigin: true
 }))
 
 app.use('/comments/', createProxyMiddleware({
-  target: 'http://localhost:9000',
+  target: 'http://localhost:4000',
   changeOrigin: true
 }))
 
 app.use('/users/', createProxyMiddleware({
-  target: 'http://localhost:9002',
+  target: 'http://localhost:4002',
   changeOrigin: true
 }))
 
-app.use('/', createProxyMiddleware({
-  target: 'http://localhost:1000',
-  changeOrigin: true
-}))
-
-app.use('/', createProxyMiddleware({
-  target: 'http://localhost:1000',
-  changeOrigin: true
-}))
+app.use('/:current', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/index.html'));
+})
 
 app.listen(port, () => {
   console.log(`Proxy server listening on http://localhost:${port}!`)
 })
-
-module.exports = app;
